f731a57dfce678b0b2dd5ea842a62a0b9a2b86d6
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-03-10 09:55:03 +0000

    add cglm source inline

--- cmake/Findcglm.cmake
+++
@@ -1,6 +0,0 @@
-find_path(CGLM_INCLUDE_DIR NAMES cglm/cglm.h HINTS /opt/homebrew/opt/cglm/include)
-find_library(CGLM_LIBRARY NAMES cglm HINTS /opt/homebrew/opt/cglm/lib)
-
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(cglm REQUIRED_VARS CGLM_LIBRARY CGLM_INCLUDE_DIR)
-mark_as_advanced(CGLM_INCLUDE_DIR CGLM_LIBRARY)
